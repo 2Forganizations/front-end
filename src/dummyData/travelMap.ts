545d0f108abf056afcef33f65c4f1f5824aea0b5
sohee-an <soan32@hanmail.net>
2023-05-06 23:26:17 +0900

    travelMapListPage

---
+++ src/dummyData/travelMap.ts
@@ -0,0 +1,20 @@
+export const dummyData = [
+  {
+    id: 1,
+    title: "함께 2인 정식 드시러 가실 분 구합니다",
+    name: "test",
+    address: "서울특별시 구로구 도곡동",
+    recruitment: 2,
+    re_sex: "여성만",
+    re_age: "연령대 상관없음",
+  },
+  {
+    id: 2,
+    title: "함께 2인 정식 드시러 가실 분 구합니다",
+    name: "test2",
+    address: "서울특별시 구로구 도곡동",
+    recruitment: 2,
+    re_sex: "여성만",
+    re_age: "연령대 상관없음",
+  },
+];
---
+++ src/pages/travelMap/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TravelMapList from "../../component/units/travelMap/travelMapList";
+
+const TravelMap = () => {
+  return (
+    <div>
+      <TravelMapList />
+    </div>
+  );
+};
+
+export default TravelMap;
