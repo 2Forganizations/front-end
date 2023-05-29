882f45d363a921e289d119aed2b1535f9ceabb60
acwell94 <acwell94@naver.com>
2023-05-29 20:32:52 +0900

    230529 작업내용 : 로그인 토큰 발급 기능

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
 const Login = () => {
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=b7d842453c8caa996a491df05f55dc7d&redirect_uri=http://localhost:3000/login/oauth2/kakao&response_type=code`;
-  // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=9d7fd424bb1d0b7f986aa5f836a06ed6&redirect_uri=http://localhost:3000/oauth&response_type=code`;
+  // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=9d7fd424bb1d0b7f986aa5f836a06ed6&redirect_uri=http://localhost:3000/login/oauth2/kakao&response_type=code`;
 
   const handledLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
