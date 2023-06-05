68f2ddc24cf0ec6567af264c908ca6331ebe2df3
acwell94 <acwell94@naver.com>
2023-06-06 00:26:41 +0900

    230606 작업내용 : getUserInfo Page 작업중 카카오 공통 버튼 작업중

---
+++ src/component/units/login/getUserInfo/GetUserInfoPage.tsx
@@ -0,0 +1,12 @@
+import KakaoLoginShortBtn from "@/component/commons/Button/KakaoLoginShortBtn";
+import * as S from "./GetUserInfoPageStyles";
+const LoginPage = () => {
+  return (
+    <S.Main>
+      <h1>Title</h1>
+      <KakaoLoginShortBtn variant="medium">hi</KakaoLoginShortBtn>
+    </S.Main>
+  );
+};
+
+export default LoginPage;
